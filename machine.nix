2da952ab9204e7aae63358ce8ab37ffe3e704ce2
Andreas Herrmann <andreash87@gmx.ch>
2014-07-20 21:11:21 +0200

    Include mydaemon into the machine

--- machine.nix
+++ machine.nix
@@ -6,8 +6,15 @@
 
     with pkgs.lib;
 
+    let
+
+      mydaemon = import ./daemon.nix { };
+
+    in
+
     {
       # Daemon
+      systemd.services.mydaemon = mydaemon;
       services.mydaemon = {
         enable = true;
         outFile = /var/mydaemon.out;
